3c04fae66d14d258e71b773900edf6f36c2b5b50
Mohammad <mohammad8758@hotmail.com>
2018-04-25 01:54:49 +0530

    Added update route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+var {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -64,8 +65,33 @@ app.delete('/todos/:id', (req, res) => {
         res.status(200).send({todos});
     }).catch((e) => {
         res.status(400).send();
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            res.status(404).send();
+        }
+        res.status(200).send({todo});
+    }).catch ((e) => {
+      res.status(400).send();  
     })
-})
+    
+});
 
 
 
