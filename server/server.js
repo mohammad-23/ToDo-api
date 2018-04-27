666eeb9b164a4b4c97d01d651520a7cabba5f4fc
Mohammad <mohammad8758@hotmail.com>
2018-04-27 07:49:53 +0530

    Added first private route

--- server/server.js
+++ server/server.js
@@ -8,12 +8,14 @@ const jwt = require('jsonwebtoken');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+//POST /todos
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -27,6 +29,7 @@ app.post('/todos', (req, res) => {
     //    console.log(req.body);
 });
 
+//GET /todos
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send(todos);
@@ -35,6 +38,7 @@ app.get('/todos', (req, res) => {
     })
 });
 
+//GET /todos by ID
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
 
@@ -55,6 +59,7 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+//DELETE /todos by ID
 app.delete('/todos/:id', (req, res) => {
     var id = req.params.id;
 
@@ -75,6 +80,7 @@ app.delete('/todos/:id', (req, res) => {
     });
 });
 
+// UPDATE /todos by ID
 app.patch('/todos/:id', (req, res) => {
     var id = req.params.id;
     var body = _.pick(req.body, ['text', 'completed']);
@@ -106,6 +112,7 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+// POST /users
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
@@ -118,6 +125,11 @@ app.post('/users', (req, res) => {
         res.header('x-auth', token).send(user);
     });
 });
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+})
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
